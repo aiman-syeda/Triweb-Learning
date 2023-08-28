ca4a69387e35781b6c43d6f6030dfbb893bc54da
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-08-28 11:01:34 +0200

    Use while loop to print series of numbers for a dynamic range

---
+++ CSS/Programming/whileloop.py
@@ -0,0 +1,10 @@
+print('To print dynamic range')
+
+#Take input from the user
+num1 = int(input('Enter start point:'))
+num2 = int(input('Enter the end point:'))
+gap = int(input('Enter the gap:'))
+#Use while loop to print the series
+while num1 ≤ num2:
+    print(num1)
+    num1+=gap
