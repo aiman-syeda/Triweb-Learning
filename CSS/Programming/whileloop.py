4f2dfa656ce8e895997481218d5aa361e5de3c88
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-08-28 18:39:07 +0200

    change ≤ to <= in while loop condition

--- CSS/Programming/whileloop.py
+++ CSS/Programming/whileloop.py
@@ -5,6 +5,7 @@ num1 = int(input('Enter start point:'))
 num2 = int(input('Enter the end point:'))
 gap = int(input('Enter the gap:'))
 #Use while loop to print the series
-while num1 ≤ num2:
+while num1 <= num2:
     print(num1)
     num1+=gap
+    
