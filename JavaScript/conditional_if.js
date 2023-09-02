2a6441283e197af1d28229ac8f9c05da4c88567e
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-02 13:46:47 +0200

    Use of nested if statement to get greater of three values

---
+++ JavaScript/conditional_if.js
@@ -0,0 +1,23 @@
+// Let us use conditional if statement to compare 3 deifferent numbers and return greater value //
+
+var x = 10;
+var y = 20;
+var z = 34;
+
+
+if (x > y) {
+    if (x > z) {
+        console.log('x is greater:' +x);
+    } else {
+        console.log('z is greater:' +z);
+    }   
+} else {
+    if (y > z) {
+        console.log('y is greater:' +y);
+    } else {
+        console.log('z is greater:' +z);
+    }
+}
+
+
+    
