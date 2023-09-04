83b20c2051d497deac82ff699a2adec540d8d3ff
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-04 09:57:56 +0200

    JavaScript is case sensitive and follows UNICODE

---
+++ JavaScript/javascript_is_case_sensitive.js
@@ -0,0 +1,11 @@
+/* JavaScript is case sensitive and follows UNICODE */
+
+var number = 23;
+var Number = 12;
+
+prüfung = 'mathe';
+prufung = 'englisch';
+
+console.log('number is  ' +number ,'Number is ' +Number);
+console.log('prüfung is  ' +prüfung ,'prufung is  ' +prufung);
+
