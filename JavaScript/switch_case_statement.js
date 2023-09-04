628b945fbd9bee503472081074a8bbdba3d05d0c
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-04 16:28:20 +0200

    Switch statement with multiple case conditions

---
+++ JavaScript/switch_case_statement.js
@@ -0,0 +1,37 @@
+let days = 7;
+
+switch(days){
+    case 1:
+        console.log('Sunday');
+        break;
+    case 3:
+        console.log('Tuesday');
+        break;
+    case 4:
+        console.log('Wednesday');
+        break;
+    case 5:
+        console.log('Thursday');
+        break;
+    case 6:
+        console.log('Friday');
+        break;
+    case 7:
+        console.log('Saturday');
+        break;
+    
+}
+let weekdays=3
+switch(weekdays){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('It is a working day');
+        break;
+    case 6:
+    case 7:
+        console.log("Hurray it's a holiday");
+        break;
+}
