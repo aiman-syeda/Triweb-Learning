c6e42da15972b0669a39f0bcae5b3f18d6937a41
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-04 15:04:28 +0200

    Different datatypes in javascript

---
+++ JavaScript/data_types.js
@@ -0,0 +1,11 @@
+var num = 3453;
+var bool = true;
+var nulltype = null;
+var undefined;
+var fname = 'John';
+
+console.log(typeof(num));
+console.log(typeof(bool));
+console.log(typeof(undefined));
+console.log(typeof(fname));
+console.log(typeof(nulltype));
