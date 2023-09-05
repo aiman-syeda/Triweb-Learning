6d227de1c5fd92ec932ada8413cb0bad3ecf1b72
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-05 16:21:00 +0200

    Based on condition the function will give addition result or subtraction result

--- JavaScript/function_expression.js
+++ JavaScript/function_expression.js
@@ -5,4 +5,22 @@ const addition = function add(num1,num2){
     console.log('sum is ',sum);
 }
 
-addition(12,3);
+addition(12,3);  
+//add(34,1);    //Throws reference error
+
+//Condition based function
+
+let result;
+let ope = 1
+
+
+if (ope == 1){
+    result = function(n1,n2){
+        console.log('Result for addition is ',+n1+n2);
+    } 
+    }else if (ope == 2){
+        result = function(n1,n2){
+        console.log('Result for addition is ',+n1-n2);
+}
+    }
+result(3,4)
