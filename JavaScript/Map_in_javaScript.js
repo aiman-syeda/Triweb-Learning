e6f4ae5e69d50cc84314783ed7123c9ff58ad652
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-11 11:08:55 +0200

    Maps in JavaScript

---
+++ JavaScript/Map_in_javaScript.js
@@ -0,0 +1,7 @@
+//Create a map 
+let map1 = new Map();
+map1.set('name','Eliana');
+console.log(map1);
+map1.set('mother','Alia');
+map1.set('father', 'john');
+console.log('Length of the map is ',+map1.size,map1);
