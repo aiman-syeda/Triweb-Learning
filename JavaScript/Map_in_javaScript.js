7c7df7793564e69af94a7d9b41241842ecee9315
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-11 11:31:24 +0200

    Weak maps are risky to use

--- JavaScript/Map_in_javaScript.js
+++ JavaScript/Map_in_javaScript.js
@@ -5,3 +5,13 @@ console.log(map1);
 map1.set('mother','Alia');
 map1.set('father', 'john');
 console.log('Length of the map is ',+map1.size,map1);
+
+//Weak map
+
+let obj = {name: 'Hi'};
+let map2 = {[obj]: 'Khazi' };
+console.log(map2[obj]);
+obj = "My Name"    //Reference changed
+let obj2 = {name: "Hi"};
+console.log(map2[obj2]);
+console.log(map2);
