0230a0a157b5028ccd80fb0128ee9809de598c8f
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-11 12:38:45 +0200

    Create a folder for concepts in ES6 and a file to understand constants

---
+++ ES6/constant.js
@@ -0,0 +1,7 @@
+//They are immutable variable but in objects the value paired with a key can be changed
+
+const obj = {name:'Ayesha',age:23};
+console.log(obj);
+obj.name = 'Alena';
+obj.age = 43;
+console.log(obj);
