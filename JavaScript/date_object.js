41d22542f9b765893d9a665ffa0e07270c89459d
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-11 09:41:05 +0200

    Date object in Javascript

---
+++ JavaScript/date_object.js
@@ -0,0 +1,11 @@
+let date = "2023 09 11 09:33:00";
+
+let dateobj = new Date(date);    //now given string is date type
+console.log(dateobj.getDate());
+console.log(dateobj.getMonth());
+console.log(dateobj.getMinutes());
+console.log(dateobj.getFullYear());
+console.log(dateobj.getDay());
+console.log(dateobj.getTime());
+
+
