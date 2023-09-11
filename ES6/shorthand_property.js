480fa6e62bc63dde7485c88369692bd3c3f76e54
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-11 15:07:37 +0200

    Shorthand property

---
+++ ES6/shorthand_property.js
@@ -0,0 +1,13 @@
+let uname = 'Akshay';
+let age = 44;
+let student = {
+    uname:uname,
+    age:age
+}
+console.log(student);
+
+let student2 = {
+    uname,
+    age,
+}
+console.log('Using short hand property',student2);
