96ad808399343da00ceb914b028be1fc0356f0bf
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-12 09:30:28 +0200

    Number methods in ecmascript

---
+++ ES6/number_method.js
@@ -0,0 +1,10 @@
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.EPSILON);
+let num1 = 1234;
+let num2 = 1.33;
+ console.log(Number.isFinite(num1));
+ console.log(Number.isInteger(num2));
+ console.log(Number.isNaN(num1));
+ 
+
