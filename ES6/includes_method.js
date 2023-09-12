f120a98be8aeafccd87a71147314509c0e06157a
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-12 08:53:44 +0200

    String methods like .includes, .startsWith, .endsWith

---
+++ ES6/includes_method.js
@@ -0,0 +1,10 @@
+//String method .includes is used to check if a string is a substring of another string
+
+str1 = 'I live in India';
+str2 = 'India';
+console.log(str1.includes(str2));
+console.log(str1.startsWith(str2));
+console.log(str1.endsWith(str2));
+
+
+
