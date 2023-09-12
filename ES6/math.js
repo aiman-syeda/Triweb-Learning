a74d1d35aefaf147eb14bbd1bc3924e1f57e4dba
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-12 09:25:54 +0200

    Some Math methods in ecmascript

---
+++ ES6/math.js
@@ -0,0 +1,13 @@
+//Math.trunc()
+
+console.log(Math.trunc(4.33));
+console.log(Math.trunc(4.63));
+ //Math.sign
+console.log(Math.sign(-9));
+//Math cuberoot
+console.log(Math.cbrt(4.33));
+//log 
+console.log(Math.log2(4.33));
+console.log(Math.log10(4.33));
+
+
