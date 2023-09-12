86adc2b2fb1d533b291f75c6d1bd422bd2b18db5
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-12 09:17:34 +0200

    .find() and .findIndex() methods in array

--- ES6/array_like_str.js
+++ ES6/array_like_str.js
@@ -11,3 +11,34 @@ function xyz(){
 
 }
 xyz(1,2,3,4);
+
+//keys
+
+let arr2 = ['a','r','h','a','m'];
+let keys = arr2.keys();
+console.log(keys);   //returns an object iterator
+
+for (let ele of keys){
+    console.log(ele);  //gives index of arr2
+}
+
+//Array .find()
+
+let x = [1,6,8,9,4,5,3];
+
+let firstnumodd = x.find(getOdd);
+
+function getOdd(value,index,ar){
+    return value % 2 != 0;
+}
+console.log(firstnumodd);
+
+//Array .findindex()
+
+console.log
+let indexval= x.findIndex((value,index,arra)=>{
+    return value % 2 != 0;
+});
+console.log(indexval);
+
+
