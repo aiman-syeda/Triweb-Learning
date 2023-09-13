d7649eabd6d9c928ac675037b15a96b51c12e39a
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-13 10:59:29 +0200

    Insert values in the rows

--- My_SQL/create_db_and_table.sql
+++ My_SQL/create_db_and_table.sql
@@ -4,7 +4,10 @@ CREATE TABLE `Raw_data`.`User_Data`;
 CREATE TABLE `Raw_data`.`User_Data` (`Id` INT NOT NULL AUTO_INCREMENT ,
 `User_Name` VARCHAR(255) NOT NULL , `Email` VARCHAR(255) NOT NULL , 
 `Password` VARCHAR(255) NOT NULL , `Remark` VARCHAR(255) NOT NULL , PRIMARY KEY (`Id`))
-INSERT INTO `User_Data` (`Id`, `User_Name`, `Email`, `Password`, `Remark`)
- VALUES ('1', 'Akshay', 'akshay@gmail.com', '1234', 'done'), 
+INSERT INTO User_Data (`Id`, `User_Name`, `Email`, `Password`, `Remark`)
+ VALUES (1, 'Akshay', 'akshay@gmail.com', '1234', 'done'), 
 (2, 'Sanjay', 'sanjay@gmail.com', '2468', 'done'),
 (3, 'Rajiv', 'rajiv@gmail.com', '1369','pending');
+
+INSERT INTO User_data (User_name,Email,Password,Remark) 
+VALUES ('Amit','amit@gmail.com','123456','done');
