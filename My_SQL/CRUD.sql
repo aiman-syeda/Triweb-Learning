6c8657d19cf122f2c70c86f413878496b293935c
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-14 10:34:53 +0200

    Insert records into the table

--- My_SQL/CRUD.sql
+++ My_SQL/CRUD.sql
@@ -28,3 +28,15 @@ ADD COLUMN class_id INT(30) NOT NULL;
 
 ALTER table students
 ADD FOREIGN KEY (class_id) REFERENCES classes(id);
+
+
+INSERT INTO teachers(mobile,teacher_name)
+ VALUES ('2345','Raju'),('33333','Basha'),('2134345','Ramesh'),('44433' ,'Razaq');
+
+INSERT INTO classes(class_name,teacher_id) 
+ VALUES ('One',2),('Two',1),('Three',2),('Two',1);
+
+INSERT into students(id,student_name,std_mobile,class,class_id) 
+ VALUES(1,'Akshay','1122',4,2),(2,'Amit','114253',3,1),(3,'Ajay','11425333',7,2);
+
+
