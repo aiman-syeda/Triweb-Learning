390178ab4b3c12b754ae85fb1ef5d354f90120ae
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-18 13:06:24 +0200

    Get the user id from the data parsed

--- BasicProjectNodeJS/models/user.js
+++ BasicProjectNodeJS/models/user.js
@@ -1,20 +1,20 @@
 
-const db = require('/Users/syeda/Documents/Aiman Syeda/Node JS app_2/util/database.js');
+const db = require('/Users/syeda/Documents/Aiman Syeda/BasicProjectNodeJS/util/database.js');
 
 
 module.exports.insert = async (userData) => {
-    
+    let id=0;  
     //handle run time exception
     try{
         let query = "INSERT INTO `Users` (`name`, `email`, `password`, `remark`) VALUES (?, ?, ?, ?)";
        
         let result = await db.execute(query,[userData.name,userData.email,userData.password,userData.remark]);
-        console.log(result);  
+        id = result[0].insertId; 
     } catch (error){
         console.log(error);
         return false;
     }
-    return true;
+    return id;
 
 }
 
