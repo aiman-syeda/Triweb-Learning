a0c623a3597590ea64ebd34ed52b50022ded232a
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-18 14:20:22 +0200

    Get user data by passing user id

--- BasicProjectNodeJS/controllers/user.js
+++ BasicProjectNodeJS/controllers/user.js
@@ -3,17 +3,23 @@ const userModel = require('/Users/syeda/Documents/Aiman Syeda/BasicProjectNodeJS
 
 //Registration
 module.exports.register = async (req,res) =>{
-    let insertedId = await userModel.insert(req.body)
+    let insertedId = await userModel.insert(req.body);
     if (insertedId > 0){
-        res.send({status:'Success',data:{id:insertedId}});
+        res.send({status:'User Registeration Successfull',data:{id:insertedId}});
     }else{
         res.send({status:'error',message:'User Registration failed'});
     }
 };
 
 //get data
-module.exports.get = (req,res) =>{
-    res.send(req.body);
+module.exports.get = async (req,res) =>{
+    let Data = await userModel.get(req.body);
+    if (Data) {
+        res.send({status:'User Data found',data:Data});
+    }else{
+        res.send({status:'error',message:'User Not Found'});
+    }
+    
 };
 
 
--- BasicProjectNodeJS/models/user.js
+++ BasicProjectNodeJS/models/user.js
@@ -3,18 +3,33 @@ const db = require('/Users/syeda/Documents/Aiman Syeda/BasicProjectNodeJS/util/d
 
 
 module.exports.insert = async (userData) => {
-    let id=0;  
+    let id = 0;
     //handle run time exception
     try{
         let query = "INSERT INTO `Users` (`name`, `email`, `password`, `remark`) VALUES (?, ?, ?, ?)";
        
         let result = await db.execute(query,[userData.name,userData.email,userData.password,userData.remark]);
         id = result[0].insertId; 
+        //console.log(id); 
     } catch (error){
         console.log(error);
         return false;
     }
     return id;
 
+}
+
+module.exports.get = async (userData) => {
+    let retData = '';
+    try {
+        let query = "SELECT * FROM Users WHERE id= ?";
+
+        let result = await db.execute(query,[userData.id]);
+        retData = result[0][0];
+    }catch (error) {
+        console.log(error);
+        return false;
+    }
+    return retData;
 }
 
