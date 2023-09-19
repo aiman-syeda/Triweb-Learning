61932916c4603e31ad2eef84088a02a7094c93fe
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-19 12:17:47 +0200

    Constructors used in class

---
+++ Node JS/concepts/constructor.js
@@ -0,0 +1,22 @@
+//Constructor is a special method of assigning properties.And it is automatically called when the object is created.
+
+class car{
+    constructor(make,model,year,color){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
+
+let car1 = new car('Mercedes','xxx',2020,'Black');
+let car2 = new car('Ford','yyyy',2021,'Grey');
+
+console.log(car1.make);
+console.log(car1.model);
+console.log(car1.year);
+console.log(car2.year);
+
+
+
+
