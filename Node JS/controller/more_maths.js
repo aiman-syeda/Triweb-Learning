fff19057862e036b0c58804030e69c17a0d035b6
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-09-20 11:26:19 +0200

    Create a url to compare 3 numbers and return  greater of three

--- Node JS/controller/more_maths.js
+++ Node JS/controller/more_maths.js
@@ -4,3 +4,23 @@ module.exports.even_odd = (req,res) => {
     }
     res.send(`${req.body.number} is an odd number`);
 }
+
+
+module.exports.compare_nums = (req,res) => {
+    let x = req.body.num1;
+    let y = req.body.num2;
+    let z = req.body.num3;
+    if (x > y){
+        if (x > z) {
+            res.send(`num1 = ${x} is greater`);
+        } else {
+            res.send(`num3 = ${z} is greater`);
+        }
+        }else{
+            if (y > z) {
+                res.send(`num2 = ${y} is greater`);
+            } else {
+                res.send(`num3 = ${z} is greater`);
+            }   
+        } 
+}
--- Node JS/routers/r_more_maths.js
+++ Node JS/routers/r_more_maths.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.post('/even-odd',mathematicalController.even_odd);
 
+router.post('/compare-numbers',mathematicalController.compare_nums);
+
 
 module.exports = router;
