e8852b15a14d38bd60ed177a94c8aac9f3ab9e55
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-11 09:53:30 +0200

    Exam start and submit functionalities added

--- Projects/Quiz_App/src/controllers/exam.ts
+++ Projects/Quiz_App/src/controllers/exam.ts
@@ -1,12 +1,59 @@
 
-import { Request,Response } from "express";
+import { NextFunction, Request,Response } from "express";
+import Quiz from "../models/quiz";
+import ProjectError from "../helper/error";
+import Result from "../models/result";
 
-const startExam = (req:Request,res:Response) => {
+const startExam = async (req:Request,res:Response,next:NextFunction) => {
+   try {
+      const quizId = req.params.quizid;
+      const quiz = await Quiz.findById(quizId,{name:1,questions_list:1,is_published:1});
+      if(!quiz){
+         const err = new ProjectError("Quiz not found!");
+         err.statusCode = 405;
+         throw err;
+      }
+      if(!quiz.is_published){
+         const err = new ProjectError("Quiz not published!");
+         err.statusCode = 405;
+         throw err;
+      }
+      res.send(quiz);
+   } catch (error) {
+      next(error);
+   }
+   
    res.send(req.params.quizid);
 };
 
-const submitExam = (req:Request,res:Response) => {
-   res.send(req.body);
+const submitExam =async  (req:Request,res:Response,next:NextFunction) => {
+   try {
+      const attempted_questions = req.body.attempted_questions;
+      const quizId = req.body.quizId;
+      const quiz = await Quiz.findById(quizId,{answers:1});
+      if(!quiz){
+         const err = new ProjectError("Quiz not found!");
+         err.statusCode = 405;
+         throw err;
+      }
+      const answers = quiz.answers;
+      const userId = req.userId;
+      const allquestions = Object.keys(answers);
+      const total = allquestions.length;
+      let score = 0;
+      for(let i = 0; i <= total; i++){
+         let question_number = allquestions[i];
+
+         if(!!attempted_questions[question_number] && attempted_questions[question_number] == answers[question_number]){
+            score += 1;
+         }
+      }
+      const result = new Result({userId,quizId,total,score});
+      const data = await result.save();
+      res.send({total,score,resultId:data._id});
+   } catch (error) {
+      next(error);
+   }
    
 };
 
