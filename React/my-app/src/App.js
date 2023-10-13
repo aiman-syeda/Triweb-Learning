8f83086a74e6e42f312d520c63dee0b9e961a7bf
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-13 15:59:50 +0200

    React app entry point,demonstrated use of props in React

---
+++ React/my-app/src/App.js
@@ -0,0 +1,14 @@
+import FirstComponent from "./components/FirstComponent";
+
+
+function App() {
+  return (
+    <div >
+      <FirstComponent name = "Aiman" topic = "props" sub = "React"/>
+      <FirstComponent name = "Anaya" topic = "Maths" sub = "Maths"/>
+
+    </div>
+  );
+}
+
+export default App;
