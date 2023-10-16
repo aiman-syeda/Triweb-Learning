2e4b01baf93258c4fff94edf9662b4f087cc549c
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-16 10:53:18 +0200

    Components of a react application

---
+++ React/my-app/src/components/Backdrop.js
@@ -0,0 +1,10 @@
+function Backdrop() {
+    return <div className="backdrop">
+        
+            <h1>React Intro</h1>
+            
+        
+    </div>
+};
+
+export default Backdrop;
--- React/my-app/src/components/FirstComponent.js
+++ React/my-app/src/components/FirstComponent.js
@@ -1,9 +1,10 @@
 function FirstComponent(props) {
-    return <div>
+    return <div className="modal">
        <h1>
        {props.name} is learning {props.topic} now
         </h1> 
-        <p>{props.sub} props are really fun</p>
+        <p>{props.sub}  is really fun</p>
+        <button onClick={props.closeModal}>close</button>
     </div>
 };
 
