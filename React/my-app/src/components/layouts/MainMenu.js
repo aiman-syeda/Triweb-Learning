0272b02b00492013ad6502969c7dc00184b668c9
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-16 14:41:57 +0200

    anchor tags replaced with Link as they avoid page reloading and rerendering

--- React/my-app/src/components/layouts/MainMenu.js
+++ React/my-app/src/components/layouts/MainMenu.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
+
 function MainMenu() {
     return <header>
-        <a href="/"> Home</a>
-        <a href="/about"> About</a>
-        <a href="/contact"> Contact</a>
+        <Link to="/"> Home</Link>
+        <Link to="/about"> About</Link>
+        <Link to="/contact"> Contact</Link>
+
     </header>
 }
 
