37d8040e6df7e33482890ac6a1c69d6f4e7757aa
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-16 14:47:45 +0200

    links wraped inside nav tag,ul is for columns and li for lists

--- React/my-app/src/components/layouts/MainMenu.js
+++ React/my-app/src/components/layouts/MainMenu.js
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 
 function MainMenu() {
     return <header>
-        <Link to="/"> Home</Link>
-        <Link to="/about"> About</Link>
-        <Link to="/contact"> Contact</Link>
+        <nav>
+            <ul>
+                <li> <Link to="/"> Home</Link></li>
+                <li> <Link to="/about"> About</Link></li>
+                <li> <Link to="/contact"> Contact</Link></li>
+            </ul>
+        </nav>
+
 
     </header>
 }
