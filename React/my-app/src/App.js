235f84be885bc9309b081d47b745924db1fb1ccb
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-16 14:36:34 +0200

    Created main menu using href inside anchor tag

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,14 +2,17 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import { Route, Routes } from 'react-router-dom';
+import MainMenu from './components/layouts/MainMenu';
 
 function App() {  
   return (
     <div >
+      <MainMenu/>
       <Routes>
         <Route path="/" element={ <Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
+
       </Routes>
      
       
---
+++ React/my-app/src/components/layouts/MainMenu.js
@@ -0,0 +1,9 @@
+function MainMenu() {
+    return <header>
+        <a href="/"> Home</a>
+        <a href="/about"> About</a>
+        <a href="/contact"> Contact</a>
+    </header>
+}
+
+export default MainMenu;
