b647d8044f1fc6f4d6ea9906e47f1b7dc7f5a80b
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-17 12:33:57 +0200

    Render dynamic  list items

--- React/my-app/src/components/Product.js
+++ React/my-app/src/components/Product.js
@@ -1,5 +1,28 @@
 function Product() {
-    return <div>Product</div>
+    let products = [
+        {
+            id: "p-1",
+            book_name: "Rich Dad poor Dad",
+            description: "know why rich peaople are becoming more rich",
+            price: "800",
+            language: "English"
+        },
+        {
+            id: "p-2",
+            book_name: "Tales from Quran",
+            description: "Brief of tales of prophets from Quran",
+            price: "400",
+            language: "English"
+        }
+    ]
+
+    return <div className="products">
+        <ul >
+            {products.map((item) => {
+                return <li key={item.id}>{item.book_name}</li>;
+            })}
+        </ul>
+    </div>
 }
 
 
