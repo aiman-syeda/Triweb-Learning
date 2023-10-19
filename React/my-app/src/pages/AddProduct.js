c7fd16d58cf1d8cc0500bc176f39343a342cc98a
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-19 13:20:56 +0200

    file formatted

--- React/my-app/src/pages/AddProduct.js
+++ React/my-app/src/pages/AddProduct.js
@@ -1,18 +1,19 @@
 import AddProductForm from "../components/products/AddProductForm";
 
-function AddProduct(){
-    function addProductHandler(newProduct){
-        console.log(newProduct);
-        fetch("http://localhost:3002/product",{
-            method:"POST",
-            body:JSON.stringify(newProduct),
-            headers:{
-                'content-Type':'application/json'
+function AddProduct() {
+    function addProductHandler(newProduct) {
+        fetch("http://localhost:3002/product", {
+            method: "POST",
+            body: JSON.stringify(newProduct),
+            headers: {
+                'content-Type': 'application/json'
             }
-
         })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
     }
-    return <div><AddProductForm addProductHandler={addProductHandler}/></div>
+    return <div><AddProductForm addProductHandler={addProductHandler} /></div>
 }
 
 
