c19e2270f0a7edcd575d9359d6a527363ac3dcc2
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-20 10:37:13 +0200

    Application wrapped in Context

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
-import {BrowserRouter} from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import { ContextFavorite } from './components/store/ContextFavourite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter><App /> </BrowserRouter>
+    <ContextFavorite>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ContextFavorite>
 );
 
 
