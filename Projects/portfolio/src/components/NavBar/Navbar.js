3fe11008d77e0a24a4e2f40e42e43429e89c4a08
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-06 10:07:12 +0100

    Added some more css and chnaged logo

--- Projects/portfolio/src/components/NavBar/Navbar.js
+++ Projects/portfolio/src/components/NavBar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import logo from "../../assets/wd-high-resolution-logo.png";
+import logo from "../../assets/as-high-resolution-logo-white-transparent (1).png";
 import { Link } from "react-scroll";
 import contactimg from "../../assets/contact.png";
  
@@ -11,8 +11,8 @@ const Navbar  = () => {
             <div className="desktopMenu" >
                 <Link className="desktopMenulistItem">Home</Link>
                 <Link className="desktopMenulistItem">About</Link>
-                <Link className="desktopMenulistItem">Portfolio</Link>
-                <Link className="desktopMenulistItem">Clients</Link>
+                <Link className="desktopMenulistItem">Projects</Link>
+                <Link className="desktopMenulistItem">Skills</Link>
             </div>
             <button className="desktopMenubtn" >
                 <img src={contactimg}  alt="Contact" className="contactimg" />Contact Me
