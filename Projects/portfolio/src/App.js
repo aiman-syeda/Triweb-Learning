2709f382bc946065de4e672e6ec2ac9da90f4b18
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-10 21:56:24 +0100

    Skill section and contact form added

--- Projects/portfolio/src/App.js
+++ Projects/portfolio/src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/NavBar/Navbar";
 import Intro from "./components/Intro/Intro";
 import About from "./components/About/About";
 import Project from "./components/Projects/Project";
+import Skill from "./components/Skills/Skill";
+import Contact from "./components/Contact/Contact";
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Intro />
       <About />
       <Project />
+      <Skill />
+      <Contact />
     </div>
   );
 }
