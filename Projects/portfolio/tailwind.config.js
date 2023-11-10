b07829639a05243d7bdf7dd7ca5febdb719559ae
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-10 21:49:56 +0100

    Added files to tailwind.config

--- Projects/portfolio/tailwind.config.js
+++ Projects/portfolio/tailwind.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   content: [
     
-    "./src/components/Projects/Project.js" 
+    "./src/components/Projects/Project.js",
+    "./src/components/Skills/Skill.js" ,
+    "./src/components/Skills/FilterSkills.js",
+    "./src/components/Contact/Contact.js"
   ],
   theme: {
     extend: {
       colors: {
         'light': '#254c5f',
-        'regular': '#163546'
+        'regular': '#163546',
+        'chocolate': '#d2691e' 
       },
       transitionDuration: {
         '3': '3s',
