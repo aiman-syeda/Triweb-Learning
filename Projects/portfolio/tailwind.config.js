01d531c56e7aae3fa29af2280e4f48c0693edd35
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 15:30:11 +0100

    Added dependencies

--- Projects/portfolio/tailwind.config.js
+++ Projects/portfolio/tailwind.config.js
@@ -5,12 +5,14 @@ module.exports = {
     "./src/components/Projects/Project.js",
     "./src/components/Skills/Skill.js" ,
     "./src/components/Skills/FilterSkills.js",
-    "./src/components/Contact/Contact.js"
+    "./src/components/Contact/Contact.js",
+    "./src/components/Footer/Footer.js"
+
   ],
   theme: {
     extend: {
       colors: {
-        'light': '#254c5f',
+        'light': '#34495E',
         'regular': '#163546',
         'chocolate': '#d2691e' 
       },
