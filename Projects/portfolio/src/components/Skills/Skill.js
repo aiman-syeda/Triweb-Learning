8fc43deb817ffd562a9cc6dd8a99b9e5e795a2aa
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 15:26:51 +0100

    Assets used in the project

--- Projects/portfolio/src/components/Skills/Skill.js
+++ Projects/portfolio/src/components/Skills/Skill.js
@@ -36,43 +36,45 @@ const Skill = () => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-      };
+    };
 
-const [items, setItems] = useState(Data);
+    const [items, setItems] = useState(Data);
 
-const filterItems = (genr) => {
-    const newItems = Data.filter((newval) => newval.genre === genr)
-    setItems(newItems);
-}
+    const filterItems = (genr) => {
+        const newItems = Data.filter((newval) => newval.genre === genr)
+        setItems(newItems);
+    }
     return (
-        <section className='w-3/4 m-auto mt-[105px]'>
-            <div className='mb-[80px]' >
-                <h1 className='text-3xl text-center font-semibold'>Skills</h1>
-                <div className='mt-12 justify-contents text-lg font-light'>
-                <p>I possess a diverse skill set that spans various aspects of web development, programming languages, and software development tools. Here's a snapshot:<br/><br/>
-                In the realm of web development, I am proficient in HTML5, CSS3, and JavaScript, with hands-on experience in Node.js, Express.js, MongoDB, SQL, React.js, and related technologies.<br/><br/>
-                My programming expertise extends to Python, Java, and C-Programming, allowing me to tackle a variety of projects.<br/><br/> 
-                Additionally, I am well-versed in essential software development and collaboration tools, including Jira Software for project management, understanding the Software Development Life Cycle (SDLC), and utilizing Git for version control.<br/><br/>
-                Familiarity with development environments such as Visual Studio Code and proficiency in database management tools like XAMPP, phpMyAdmin, SQLite, and Postman further enhance my capabilities. </p>
+        <section className='skill'>
+            <section className='w-3/4 m-auto mt-[105px]'>
+                <div className='mb-[80px]' >
+                    <h1 className='text-3xl text-center font-semibold'>Skills</h1>
+                    <div className='mt-12 justify-contents text-lg font-light'>
+                        <p>I possess a diverse skill set that spans various aspects of web development, programming languages, and software development tools. Here's a snapshot:<br /><br />
+                            In the realm of web development, I am proficient in HTML5, CSS3, and JavaScript, with hands-on experience in Node.js, Express.js, MongoDB, SQL, React.js, and related technologies.<br /><br />
+                            My programming expertise extends to Python, Java, and C-Programming, allowing me to tackle a variety of projects.<br /><br />
+                            Additionally, I am well-versed in essential software development and collaboration tools, including Jira Software for project management, understanding the Software Development Life Cycle (SDLC), and utilizing Git for version control.<br /><br />
+                            Familiarity with development environments such as Visual Studio Code and proficiency in database management tools like XAMPP, phpMyAdmin, SQLite, and Postman further enhance my capabilities. </p>
 
+                    </div>
                 </div>
-            </div>
-            <FilterSkills 
-              FilterBtn={FilterBtn}
-              filterItems={filterItems}
-                            />
-            <div className='w-3/4 m-auto m-5 '>
+                <FilterSkills
+                    FilterBtn={FilterBtn}
+                    filterItems={filterItems}
+                />
+                <div className='w-3/4 m-auto m-5 '>
                     <Slider {...settings}>
-                    {items.map((item) => 
-                        <div className='mx-16 my-16 w-32 rounded-xl justify-center items-center bg-white' >
-                            <h3 className='text-black text-center justify-center items-center text-base mt-4 text-xl'>
-                                {item.title}
-                            </h3>
-                            <img src={item.image} alt='' className='object-cover mx-20 mb-7 justify-center items-center w-28 h-28' />
-                        </div>
-                    )}</Slider>
+                        {items.map((item) =>
+                            <div className='mx-16 my-16 w-32 rounded-xl justify-center items-center bg-white' >
+                                <h3 className='text-black text-center justify-center items-center text-base mt-4 text-xl'>
+                                    {item.title}
+                                </h3>
+                                <img src={item.image} alt='' className='object-cover mx-20 mb-7 justify-center items-center w-28 h-28' />
+                            </div>
+                        )}</Slider>
                 </div>
 
+            </section>
         </section>
     )
 }
@@ -116,46 +118,56 @@ const Data = [
     {
         genre: 'Web Development',
         image: retjs,
-        title: 'ReactJS'},
+        title: 'ReactJS'
+    },
     {
         genre: 'Web Development',
         image: xampp,
-        title: 'XAMPP'},
+        title: 'XAMPP'
+    },
     {
         genre: 'Web Development',
         image: pman,
-        title: 'Postman'},
+        title: 'Postman'
+    },
     {
         genre: 'Programming Languages',
         image: python,
-        title: 'Python'}, 
+        title: 'Python'
+    },
     {
         genre: 'Programming Languages',
         image: jva,
-        title: 'JAVA'}, 
+        title: 'JAVA'
+    },
     {
         genre: 'Programming Languages',
         image: cp,
-        title: 'C-Programming'},
+        title: 'C-Programming'
+    },
     {
         genre: 'Development Tools and Practices',
         image: git,
-        title: 'Git'},
+        title: 'Git'
+    },
     {
         genre: 'Development Tools and Practices',
         image: jira,
-        title: 'Jira'}, 
+        title: 'Jira'
+    },
     {
         genre: 'Development Tools and Practices',
         image: sdlc,
-        title: 'SDLC'}, 
+        title: 'SDLC'
+    },
     {
         genre: 'Development Tools and Practices',
         image: vs,
-        title: 'VS-Code'}
-    ]
+        title: 'VS-Code'
+    }
+]
 
 
 
-const FilterBtn = [...new Set(Data.map((val)=> val.genre))]
+const FilterBtn = [...new Set(Data.map((val) => val.genre))]
 export default Skill;
