50a5f96101828e6fe5d3bf4f4114e6b955231a3f
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 09:43:57 +0100

    Added a GIF to the intro page

--- Projects/portfolio/src/components/Intro/Intro.js
+++ Projects/portfolio/src/components/Intro/Intro.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Intro.css';
 import downloadiconbtn from '../../assets/-file-download_90117.png';
 import resume from '../../assets/CV_Aiman_Syeda_Word.docx';
+import introgif from '../../assets/gifintro2.gif';
 
 const Intro = () => {
   return (
@@ -13,7 +14,9 @@ const Intro = () => {
           I would <br />love to grab an opportunity
           where I can apply my skills<br /> and dive deeper into this fantastic field.  </p>
         <a href={resume} download='Resume'><button className='hiremeBtn'><img src={downloadiconbtn} className='downloadImg' alt='' />Resume</button></a>
-
+      </div>
+      <div className='introimg'>
+        <img src={introgif} alt='intro' />
       </div>
     </section>
   )
