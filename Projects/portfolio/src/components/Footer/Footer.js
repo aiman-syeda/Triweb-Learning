814cdaa1cc80d6aa47743ed660c77cda211e885d
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 16:30:21 +0100

    Footer icons changed

--- Projects/portfolio/src/components/Footer/Footer.js
+++ Projects/portfolio/src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import gitimg from '../../assets/github (1).png';
-import inimg from '../../assets/linkedin.png';
-import twitterimg from '../../assets/twitter.png';
+import gitimg from '../../assets/github (2).png';
+import inimg from '../../assets/linkedin (1).png';
+import twitterimg from '../../assets/twitter (1).png';
 
 const Footer = () => {
   return (
     <div className='h-24 w-full bg-light'>
     <footer className='flex w-3/6 mx-28 ml-96 justify-between' >
-      <a href='https://github.com/aiman-syeda'><img className='object-cover w-20 h-20 ml-28' src={gitimg} alt='' /></a>
+      <a href='https://github.com/aiman-syeda'><img className='object-cover w-20 h-20 my-2 ml-24' src={gitimg} alt='' /></a>
         
       <a href='https://www.linkedin.com/in/syeda-aiman-791188268/'><img className='object-cover w-20 h-20 my-2' src={inimg} alt='' /></a>
       <a href='https://twitter.com/SyedaAiman81647'><img className='object-cover w-20 h-20 my-2 ' src={twitterimg} alt='' /></a>
