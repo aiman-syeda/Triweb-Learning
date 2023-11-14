bfd6f5f37316249390bc9749fc5e0d51dc86736c
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 09:46:08 +0100

    Added proper navigation and changed background color

--- Projects/portfolio/src/components/NavBar/Navbar.js
+++ Projects/portfolio/src/components/NavBar/Navbar.js
@@ -9,12 +9,14 @@ const Navbar  = () => {
         <nav className="navbar">
             <img src={logo} alt="Logo" className="logo" />
             <div className="desktopMenu" >
-                <Link className="desktopMenulistItem">Home</Link>
-                <Link className="desktopMenulistItem">About</Link>
-                <Link className="desktopMenulistItem">Projects</Link>
-                <Link className="desktopMenulistItem">Skills</Link>
+                <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenulistItem">Home</Link>
+                <Link activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenulistItem">About</Link>
+                <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenulistItem">Projects</Link>
+                <Link activeClass="active" to="skill" spy={true} smooth={true} offset={-100} duration={500} className="desktopMenulistItem">Skills</Link>
             </div>
-            <button className="desktopMenubtn" >
+            <button className="desktopMenubtn" onClick={() => {
+                document.getElementById('contactme').scrollIntoView({behaviour: 'smooth'})
+            }} >
                 <img src={contactimg}  alt="Contact" className="contactimg" />Contact Me
             </button>
 
