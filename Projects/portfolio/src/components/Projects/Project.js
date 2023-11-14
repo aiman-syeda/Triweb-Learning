676ad9023f6c20982746176f2558f114f2d4c532
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-14 15:31:38 +0100

    added gitignore and modified project section

--- Projects/portfolio/src/components/Projects/Project.js
+++ Projects/portfolio/src/components/Projects/Project.js
@@ -15,41 +15,43 @@ const Project = () => {
         speed: 500,
         slidesToShow: 2,
         slidesToScroll: 1,
-      };
+    };
     return (
-        <div className='w-3/4 m-auto mt-[105px]'>
-          
-            <div className='mb-[80px]'>
-                <h1 className='text-3xl text-center font-semibold '>Projects</h1>
-                <p className='mt-[80px] mb-[80px] font-light justify-center text-lg'>Here are some of my projects.These projects highlight my expertise in web development and programming,
-                     covering a range of functionalities and applications.These experiences have not only sharpened my development abilities but also allowed me to explore different aspects of web development.</p>
+        
+            <div id='projects' className='w-3/4 m-auto mt-[105px]'>
+
+                <div className='mb-[80px]'>
+                    <h1 className='text-3xl text-center font-semibold '>Projects</h1>
+                    <p className='mt-[80px] mb-[80px] font-light justify-center text-lg'>Here are some of my projects.These projects highlight my expertise in web development and programming,
+                        covering a range of functionalities and applications.These experiences have not only sharpened my development abilities but also allowed me to explore different aspects of web development.</p>
+                </div>
+
+
+                <div className='mt-20 mb-[80px] mx-32'>
+                    <Slider {...settings}>
+                        {Projects.map((item) =>
+                            <div className='h-[300px] bg-light text-white rounded-xl m-8 transition-all duration:3 hover:scale-110 border-white'>
+
+                                <div className='text-center justify-center items-center text-base mt-4 text-xl'>
+                                    <p>{item.title}</p>
+                                </div>
+
+                                <div className='h-56 rounded-t-xl flex justify-center items-center' >
+                                    <img src={item.image} alt='' className='h-44 w-44' />
+                                </div>
+
+                                <div className='flex flex-row justify-center items-center gap-4 p-2 text-x'>
+                                    <a href={item.linkCode} target='_blank' rel='noreferrer'>Code</a>
+                                    <a href={item.linkHosted} target='_blank' rel='noreferrer'>Live</a>
+                                </div>
+
+                            </div>
+                        )}
+                    </Slider>
+                </div>
+
             </div>
-
-
-            <div className='mt-20 mb-[80px]'>
-                <Slider {...settings}>
-                {Projects.map((item) =>
-                    <div className='h-[300px] bg-light text-white rounded-xl m-4 transition-all duration:3 hover:scale-110 border-white'>
-
-                        <div className='text-center justify-center items-center text-base mt-4 text-xl'>
-                            <p>{item.title}</p>
-                        </div>
-                    
-                        <div className='h-56 rounded-t-xl flex justify-center items-center' >
-                            <img src={item.image} alt='' className='h-44 w-44' />
-                        </div>
-
-                        <div className='flex flex-row justify-center items-center gap-4 p-2 text-xs'>
-                            <a href={item.linkCode} target='_blank' rel='noreferrer'>Code</a>
-                            <a href={item.linkHosted} target='_blank' rel='noreferrer'>Live</a>
-                        </div>
-
-                    </div>
-                )}
-                </Slider>
-            </div>
-
-        </div>
+        
     )
 }
 const Projects = [
