77ed6e83f090f65fb4a89c441ca330f52c0d6a38
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-11-15 11:56:47 +0100

    Changed Resume from .docx to .pdf

--- Projects/portfolio/src/components/Intro/Intro.js
+++ Projects/portfolio/src/components/Intro/Intro.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Intro.css';
 import downloadiconbtn from '../../assets/-file-download_90117.png';
-import resume from '../../assets/CV_Aiman_Syeda_Word.docx';
+import resume from '../../assets/CV_Aiman_Syeda_Word.pdf';
 import introgif from '../../assets/gifintro2.gif';
 
 const Intro = () => {
@@ -13,7 +13,7 @@ const Intro = () => {
         <p className='introPara' >I am getting passionate about improving and growing <br />myself in the field of web development.
           I would <br />love to grab an opportunity
           where I can apply my skills<br /> and dive deeper into this fantastic field.  </p>
-        <a href={resume} download='Resume'><button className='hiremeBtn'><img src={downloadiconbtn} className='downloadImg' alt='' />Resume</button></a>
+        <a href={resume} download='Aiman-Syeda-Resume'><button className='hiremeBtn'><img src={downloadiconbtn} className='downloadImg' alt='' />Download Resume</button></a>
       </div>
       <div className='introimg'>
         <img src={introgif} alt='intro' />
